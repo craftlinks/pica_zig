9f8b7a2aa96fa515456902d2e66ef45c1af76f3b
craftlinks <craftlinks@protonmail.com>
2022-05-12 17:44:27 +0200

    202205121744

--- src/example.zig
+++ src/example.zig
@@ -3,38 +3,14 @@ const pica = @import("pica");
 
 pub fn main() !void {
 
-    std.debug.print("Hello World!\n", .{});
-    pica.print_hello();
+    const window_attributes = pica.WindowAttributes {
+            .title = "New Window",
+            .position = .{100, 100},
+            .size = .{400, 400},
+        };
 
-
+    const window = pica.Window.newWithAttributes(&window_attributes);
+    std.debug.print("window title: {s}\n", .{window.attributes.title});
+    std.debug.print("window position: {any}\n", .{window.attributes.position});
+    std.debug.print("window size: {any}\n", .{window.attributes.size});  
 }
-
-//     const winclass = w32.user32.WNDCLASSEXA{
-//         .style = 0,
-//         .lpfnWndProc = processWindowMessage,
-//         .cbClsExtra = 0,
-//         .cbWndExtra = 0,
-//         .hInstance = @ptrCast(w32.HINSTANCE, w32.kernel32.GetModuleHandleW(null)),
-//         .hIcon = null,
-//         .hCursor = w32.LoadCursorA(null, @intToPtr(w32.LPCSTR, 32512)),
-//         .hbrBackground = null,
-//         .lpszMenuName = null,
-//         .lpszClassName = @ptrCast(w32.LPCSTR, "HelloWorld"),
-//         .hIconSm = null,
-//     };
-//     _ = try w32.user32.registerClassExA(&winclass);
-
-
-// }
-
-// fn processWindowMessage(
-//     window: w32.HWND,
-//     message: w32.UINT,
-//     wparam: w32.WPARAM,
-//     lparam: w32.LPARAM,
-// ) callconv(w32.WINAPI) w32.LRESULT {
-
-//     return w32.user32.defWindowProcA(window, message, wparam, lparam);
-
-// }
-    
--- src/lib.zig
+++ src/lib.zig
@@ -3,6 +3,35 @@ pub const zwin32 = @import("zwin32");
 pub const base = @import("zwin32.base");
 
 
-pub fn print_hello() void {
-    std.debug.print("Hello, world, but from PiCaLib!\n", .{});
-}
+pub const WindowAttributes = struct {
+
+    title: []const u8 = "PiCaLib Window",
+    position: [2]i32 = .{50, 50},
+    size: [2]i32 = .{640, 480},
+};
+
+pub const Window = struct {
+
+    attributes: WindowAttributes = .{},
+
+
+    pub fn new() Window {
+        
+        const window_attributes =  .{};
+
+        const window = Window.newWithAttributes(&window_attributes);
+
+        return window;
+    
+    }
+
+    pub fn newWithAttributes (comptime window_attributes: *const WindowAttributes) Window {
+        const window: Window = .{
+            .attributes = window_attributes.*,
+        }; 
+        return window;
+    
+    }
+
+};
+
